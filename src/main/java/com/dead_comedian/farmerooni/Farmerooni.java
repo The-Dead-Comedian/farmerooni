98aa376f5c17a0be1191a52fdac4702055e2e9dd
The-Dead-Comedian <113227369+The-Dead-Comedian@users.noreply.github.com>
2026-06-24 23:55:26 -0400

    Initial Commit

---
+++ src/main/java/com/dead_comedian/farmerooni/Farmerooni.java
@@ -0,0 +1,34 @@
+package com.dead_comedian.farmerooni;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+@Mod(Farmerooni.MODID)
+public class Farmerooni {
+    public static final String MODID = "farmerooni";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Farmerooni(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+}
